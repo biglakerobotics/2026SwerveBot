6af1e07660ded7c226b3b4fa4cf8a404b1dd5404
Big Lake Robotics <biglakerobotics@gmail.com>
2026-02-09 18:35:11 -0600

    added constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,12 @@
 package frc.robot;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+
 public class Constants {
     public static final double MAX_FORWARD_SPEED = 6;
     public static final double MAX_ROTATION_SPEED = 3;
@@ -26,5 +33,20 @@ public class Constants {
     public static final double softForwardLimitClimber = 1000;
     public static final double softReverseLimitClimber = 1000;
     public static final double INTAKEJOINTSPEED = 0;
-     
+
+    public static class VisionConstants {
+        public static final String LIMELIGHT_NAME1 = "limelight";
+        public static final String LIMELIGHT_NAME2 = "limelight";
+        public static final String LIMELIGHT_NAME3 = "limelight";
+        public static final String LIMELIGHT_NAME4 = "limelight";
+
+        public static final AprilTagFieldLayout APRIL_TAG_FIELD_LAYOUT = AprilTagFieldLayout
+        .loadField(AprilTagFields.k2026RebuiltWelded);
+
+        public static final Matrix<N3, N1> SINGLE_TAG_STD_DEV = VecBuilder.fill(0.1, 0.1, 0.1);
+        public static final Matrix<N3, N1> MULTI_TAG_STD_DEV = VecBuilder.fill(0.1, 0.1, 0.1);
+   
+        public static final String[] CAMERA_NAMES = {LIMELIGHT_NAME1, LIMELIGHT_NAME2, LIMELIGHT_NAME3, LIMELIGHT_NAME4};
+    }
 }
+
