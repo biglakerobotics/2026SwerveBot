5881b6206d2f664ad605978081b0a5a7cf5a11ea
Ben Lauderbaugh <benjlaud1@gmail.com>
2026-03-09 22:18:45 -0500

    starting code

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -41,43 +41,6 @@ public class Climber implements Subsystem {
     double climberSpeed = CLIMBER_MOTOR_CONFIGS.climberSpeed;
 
     public void ClimberConfiguration() {
-        climberConfigs.Slot0.kP = CLIMBER_MOTOR_CONFIGS.CLIMBERVOLTS_P_VALUE;
-        climberConfigs.Slot0.kI = CLIMBER_MOTOR_CONFIGS.CLIMBERVOLTS_I_VALUE;
-        climberConfigs.Slot0.kD = CLIMBER_MOTOR_CONFIGS.CLIMBERVOLTS_D_VALUE;
-
-        climberConfigs.Voltage.withPeakForwardVoltage(Volts.of(Constants.peakVoltage))
-            .withPeakReverseVoltage(Volts.of(-Constants.peakVoltage));
-
-        climberConfigs.Slot1.kP = CLIMBER_MOTOR_CONFIGS.CLIMBERTORQUE_P_VALUE;
-        climberConfigs.Slot1.kI = CLIMBER_MOTOR_CONFIGS.CLIMBERTORQUE_I_VALUE;
-        climberConfigs.Slot1.kD = CLIMBER_MOTOR_CONFIGS.CLIMBERTORQUE_D_VALUE;
-
-        climberConfigs.MotorOutput.withInverted(InvertedValue.CounterClockwise_Positive);
-        climberConfigs.CurrentLimits.withStatorCurrentLimit(Constants.peaKAmps).withStatorCurrentLimitEnable(true);
-        climberConfigs.SoftwareLimitSwitch.withForwardSoftLimitEnable(true).withForwardSoftLimitThreshold(Constants.softForwardLimitClimber);
-        climberConfigs.SoftwareLimitSwitch.withReverseSoftLimitEnable(true).withReverseSoftLimitThreshold(Constants.softReverseLimitClimber);
-
-        climberConfigs.TorqueCurrent.withPeakForwardTorqueCurrent(Amps.of(Constants.peaKAmps))
-            .withPeakReverseTorqueCurrent(Amps.of(Constants.peaKAmps));
-
-        StatusCode statusLead = StatusCode.StatusCodeNotInitialized;
-        StatusCode statusFollow = StatusCode.StatusCodeNotInitialized;
-        for (int i = 0; i < 5; ++i ) {
-            statusLead = climberLead.getConfigurator().apply(climberConfigs);
-            // statusLead = climberFollow.getConfigurator().apply(climberConfigs);
-            if (statusLead.isOK() && !statusFollow.isOK()) break;
-        }
-        if (!statusLead.isOK() && !statusFollow.isOK()) {
-            System.out.println("Could not apply configs to lead, error code: " + statusLead.toString());
-            System.out.println("Could not apply configs to follow, error code: " + statusFollow.toString());
-        }
-
-        climberLead.setPosition(Constants.startPosition);
-        // climberFollow.setPosition(Constants.startPosition);
-
-        // climberFollow.setControl(new Follower(climberLead.getDeviceID(), MotorAlignmentValue.Aligned));
-            
-        
     }
 
     public Climber(){
@@ -86,59 +49,6 @@ public class Climber implements Subsystem {
         System.out.println(climberFollowSwitch.get());
         System.out.println(climberLeadSwitch.getChannel());
         System.out.println(climberFollowSwitch.getChannel());
-    }
-
-
-
-    public boolean ClimberStartPos() {
-        if (!climberLeadSwitch.get()) {
-            climberLead.set(-climberSpeed);
-        } else {
-            ClimberStop();
-            climberLead.setPosition(0);
-            // climberFollow.setPosition(0);
-            m_climberLeadZeroTrue = true; 
-            System.out.println("\n Hit LimitSwitch Setting Position To Zero");
-        }
-        return m_climberLeadZeroTrue;
-    }
-
-    public void ClimberSwitch() {
-        System.out.println("Start activated: " + climberLeadSwitch.get());
-    //    if(climberSpeed < 0) {
-            System.out.println("If statement passed");
-            // if (getLeadLimitSwitch()) {
-            //     System.out.println("second if statment passed");
-            //     climberSpeed *= -1;  
-            //     climberLead.set(climberSpeed);
-            // } else {
-                // System.out.println("else statment passed");
-                climberLead.set(climberSpeed);      
-            // }
-    //    } else{
-    //     System.out.println("start of followswitch code");
-        // if (getLeadLimitSwitch()) {
-        //     System.out.println("followswitch If statement passed");
-        //     climberSpeed *= -1;
-        //     climberLead.set(climberSpeed);
-        //    } else {
-        //     System.out.println("Followswitch else statement passed");
-        //       climberLead.set(climberSpeed);
-        //    }
-    //    }
-    }
-    
-    public void ClimberStop() {
-        climberLead.set(0);
-    }
-    public void SoftLimitDisable() {
-        climberConfigs.SoftwareLimitSwitch.withForwardSoftLimitEnable(false);
-        climberConfigs.SoftwareLimitSwitch.withReverseSoftLimitEnable(false);
-        climberLead.getConfigurator().apply(climberConfigs);
-    }
-
-    public boolean getLeadLimitSwitch() {
-        return climberLeadSwitch.get();
     }
 }
 
